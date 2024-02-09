35c55d169a070d3813f5da0afbb0d6b00e722f26
JorgenMJobloop <jorgen@jobloop.no>
2024-02-09 11:19:35 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 const myText = document.getElementById("my-text");
 const myButton = document.getElementById("my-btn1");
 const newButton = document.getElementById("my-btn2");
+const userText = document.getElementById("users");
+
+const myObject = {
+    "Username": ["Per", "Ole", "Kari"]
+}
 
 // myNumber ligger i global-scope
 let myNumber = 0;
-
 
 // myNumber++ tilsvarer 0 + 1 + 1... osv.
 myButton.addEventListener("click", () => {
@@ -20,5 +24,21 @@ newButton.addEventListener("click", () => {
     counter = "Counter = " + 0;
     return myText.textContent = counter;
 });
+
+console.log(myObject);
+for (let i = 0; i < myObject.Username.length; i++) {
+    console.log(myObject, i);
+}
+userText.textContent = `${myObject.Username.reverse()} is logged in!`;
+
+// vi kan bruke async funksjoner til å kalle til en API for å hente data.
+async function getData() {
+    const response = await fetch("src/mockData.json");
+    const data = await response.json();
+    console.log(data);
+    myText.textContent = JSON.stringify(data);
+}
+
+getData();
 
 console.log(`${myNumber} DEBUG LINE: 14`);
